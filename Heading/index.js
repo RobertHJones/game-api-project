33505c6dccf84027f0fe6380704cbff68b334b1f
RobertHJones <roberthuwjones@outlook.com>
2022-02-04 20:51:16 +0000

    refactor

--- Heading/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import css from "./Heading.module..css";
-
-const { header } = css;
-
-export default function Heading() {
-  return <h1 className={header}>The place to be for checking game prices</h1>;
-}
--- src/Components/Heading/index.js
+++ src/Components/Heading/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import css from "./Heading.module..css";
-
-const { header } = css;
+import "./index.css";
 
 export default function Heading() {
-  return <h1 className={header}>The place to be for checking game prices</h1>;
+  return <h1>The place to be for checking game prices</h1>;
 }
