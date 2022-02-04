e0f0588c5ea70645661a0b3d0d92acbf6b6bc99e
RobertHJones <roberthuwjones@outlook.com>
2022-02-04 21:15:43 +0000

    basic search added

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,12 +1,29 @@
 import "./App.css";
 import Heading from "../Heading";
 import Input from "../Input";
+import { useState } from "react";
 
 function App() {
+  const [gameInfo, setGameInfo] = useState([]);
+
+  async function fetchData(game) {
+    const response = await fetch(
+      `https://www.cheapshark.com/api/1.0/games?title=${game}`
+    );
+    const data = await response.json();
+    setGameInfo(data.splice(0, 10));
+    console.log(data.splice(0, 10));
+  }
+
   return (
     <div>
       <Heading />
-      <Input />
+      <Input onSubmit={fetchData} />
+      <div>
+        <h2>{gameInfo[0].external}</h2>
+        <img src={gameInfo[0].thumb} alt="" />
+        <p>The current cheapest price is £{gameInfo[0].cheapest}</p>
+      </div>
     </div>
   );
 }
