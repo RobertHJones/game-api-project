558e48df1de39ad3dd05eeba6a0be3f70d47d2f9
RobertHJones <roberthuwjones@outlook.com>
2022-02-11 21:45:17 +0000

    larger button

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -4,7 +4,7 @@ import css from "./Login.module.css";
 import "antd/dist/antd.css";
 import { Button } from "antd";
 
-const { loginPage, header } = css;
+const { loginPage, header, button } = css;
 
 const Login = () => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
@@ -21,8 +21,12 @@ const Login = () => {
         </Button>
       ) : (
         <div className={loginPage}>
-          <h1 className={header}>Please log in to view these deals</h1>
-          <Button type="primary" onClick={() => loginWithRedirect()}>
+          <h1 className={header}>Please log in to access these deals</h1>
+          <Button
+            className={button}
+            type="primary"
+            onClick={() => loginWithRedirect()}
+          >
             Log in
           </Button>
         </div>
